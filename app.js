d0aec4025bf17ea3e55a980bba3639c8b2ab7552
Jaakko Salonen <jaakko.salonen@iki.fi>
2021-04-10 12:44:29 +0300

    add cors

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const express = require('express');
+const cors = require('cors');
 const path = require('path');
 const PORT = process.env.PORT || 5000;
 const csv = require('csv-parser')
 const fs = require('fs');
 
 const app = express();
+
+app.use(cors());
 app.use(express.static(path.join(__dirname, 'public/build')));
 
 const filesToRead = [
@@ -26,8 +29,6 @@ filesToRead.forEach(element => {
     // console.log(foodData[`${element}`]);
   });
 });
-
-
 
 app.get('/api/', (req, res) => {
   res.writeHead(200, {'Content-Type': 'application/JSON'});
