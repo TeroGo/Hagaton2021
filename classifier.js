dc2f3486e1d5fb5c5db76a7208b9dab923c418eb
Jaakko Salonen <jaakko.salonen@iki.fi>
2021-04-10 13:52:50 +0300

    fix

--- classifier.js
+++ classifier.js
@@ -1,5 +1,30 @@
+const natural = require('natural');
+const csv = require('csv-parser');
+const fs = require('fs')
+const path = require('path');
+
+const classifier = new natural.BayesClassifier();
+const results = [];
+let classifierTrained = false;
+
+fs.createReadStream(path.join(__dirname, 'data/classifier/trainingset-1.csv'))
+  .pipe(csv({ separator: ';', headers: ['classification', 'text'] }))
+  .on('data', (data) => results.push(data))
+  .on('end', () => {
+    results.forEach(({ classification, text }) => {
+      classifier.addDocument(text, classification);
+    });
+    classifier.train();
+    classifierTrained = true;
+    console.log('Finished training classifier');
+  });
+
 module.exports = {
   classify: function(text) {
-    return 'SHELLFISH';
+    if(classifierTrained) {
+      return classifier.classify(text);
+    } else {
+      return 'Classifier not trained';
+    }
   }
 };
