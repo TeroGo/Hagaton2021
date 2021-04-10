18f8de967af21fddd0d8b87a3ea30b656e86d991
Tero Paavolainen <tero.paavolainen@gofore.com>
2021-04-10 13:47:01 +0300

    fixing merge conflict with package-lock.json

--- app.js
+++ app.js
@@ -30,30 +30,79 @@ filesToRead.forEach(element => {
   });
 });
 
+const carbDataField = 'CHOAVL';
+const fatDataField = 'FAT';
+const proteinDataField = 'PROT';
+const energyDataField = 'ENERC';
+const componentAmountDataField = 'BESTLOC';
+
+const handleFoodComponentData = (object, summary, foodItem) => {
+  const findComponentType = (id, dataField) => {
+    const row = foodData.component_value.find(component =>
+      component.FOODID === id &&
+      component.EUFDNAME === dataField);
+    
+    const amount = row[`${componentAmountDataField}`].replace(',', '.');
+    return parseFloat(amount);
+  }
+  // Repetitio mater studiorum est, or something like that?
+  const carbs = findComponentType(foodItem.FOODID, carbDataField);
+  const fat = findComponentType(foodItem.FOODID, fatDataField);
+  const proteins = findComponentType(foodItem.FOODID, proteinDataField);
+  const energy = findComponentType(foodItem.FOODID, energyDataField);
+
+  object.fat = fat;
+  object.carbs = carbs;
+  object.protein = proteins;
+  object.calories = energy;
+  summary.fat += fat;
+  summary.carbs += carbs;
+  summary.protein += proteins;
+  summary.calories += energy;
+};
+
+const polishSummaryFields = (summary, ingredients, weight, fields) => {
+  fields.forEach(element => {
+     summary[`${element}`] = summary[`${element}`] / ingredients * (weight / 100.0);
+  });
+};
 app.get('/api/', (req, res) => {
   res.writeHead(200, {'Content-Type': 'application/JSON'});
   const queryText = req.query.text;
-  const foundIngredients = [];
-
-  if (queryText) {
-    foodData.food.forEach(element => {
-      const regex = new RegExp(`.*${element.FOODNAME}.*`, 'i');
-      if(queryText.match(regex)) {
-        console.log('found ' + element);
-        foundIngredients.push(element);
-      }
-    });
+  if(!queryText){
+    res.status(400);
+    res.end('Provide queryText');
   }
-  console.log(queryText);
+  const weight = req.query.weight || 100;
+  const foundIngredients = [];
+  const summary = {
+    name: queryText,
+    calories: 0,
+    carbs: 0,
+    fat: 0,
+    protein: 0
+  };
+
+  foodData.food.forEach(element => {
+    const regex = new RegExp(`.*${element.FOODNAME}.*`, 'i');
+    if(queryText.match(regex)) {
+      const ingredient = {
+        name: element.FOODNAME,
+        calories: 0,
+        carbs: 0,
+        fat: 0,
+        protein: 0
+      };
+      handleFoodComponentData(ingredient, summary, element);
+
+      console.log(ingredient);
+      foundIngredients.push(ingredient);
+    }
+  });
+  polishSummaryFields(summary, foundIngredients.length, weight, ['fat', 'carbs', 'protein', 'calories']);
 
   res.end(JSON.stringify({
-    summary: {
-      name: queryText,
-      calories: 300,
-      carbs: 25,
-      fat: 50,
-      protein: 10
-    },
+    summary,
     ingredients: foundIngredients
   }));
 });
