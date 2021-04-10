9ebe82cafdc18ab563dae4441191dbb82ad30ddc
Jaakko Salonen <jaakko.salonen@iki.fi>
2021-04-10 11:21:14 +0300

    build

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const PORT = process.env.PORT || 5000;
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public/build')));
 
 app.use((req, res) => {
   res.writeHead(200, {'Content-Type': 'application/JSON'});
--- build-and-deploy.sh
+++ build-and-deploy.sh
@@ -3,6 +3,4 @@ npm ci
 npm run build
 cp -R build ../public
 cd -
-git add .
-git commit -m "build and deploy"
 git push heroku
